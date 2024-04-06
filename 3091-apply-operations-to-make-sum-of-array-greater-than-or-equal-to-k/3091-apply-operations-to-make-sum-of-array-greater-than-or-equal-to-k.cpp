3a8cbc85d25dd473f00b63bea4cc1b75ca5f7df7
Abhay Singh <109303835+abhay-59@users.noreply.github.com>
2024-04-07 04:00:32 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (82.26%) - LeetHub

--- 3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
+++ 3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int minOperations(int k) {
-        int ans = 1e6;
-        for(int i = 1;i <= k; i++) {
-            int d = ceil(k / float(i * 1));
-            ans = min(ans, (i-2)+d);
-        }
-        return ans;
+        int a = sqrt(k);
+        return a + (k - 1) / a - 1;
     }
 };
