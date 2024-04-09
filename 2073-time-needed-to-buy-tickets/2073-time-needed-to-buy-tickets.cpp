68b0c356a8c31da145f532ef3c58f8921bf67524
Abhay Singh <109303835+abhay-59@users.noreply.github.com>
2024-04-09 23:42:50 +0530

    Time: 4 ms (41.19%), Space: 9.5 MB (46.11%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
+        ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
         int n=tickets.size(),count=0;
         while(tickets[k]>0){
         for(int i=0;i<n;i++){
