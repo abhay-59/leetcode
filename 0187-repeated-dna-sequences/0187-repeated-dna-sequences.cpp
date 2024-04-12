bc304613379a448f9e2225e7391254699899edd1
Abhay Singh <109303835+abhay-59@users.noreply.github.com>
2024-04-12 11:56:21 +0530

    Time: 40 ms (69.01%), Space: 27 MB (22.89%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
+        ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
         int n=s.size();
         vector<string>v;
         unordered_map<string,int>mp;
