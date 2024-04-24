aed4d35c061a3d7fb320a6ed221257a1b311b311
Abhay Singh <109303835+abhay-59@users.noreply.github.com>
2024-04-24 16:50:50 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (29.12%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    //vector<int>dp(38,-1);
+    int tribonacci(int n) {
+        vector<int>ans(n+3,0);
+        ans[0] = 0;
+        ans[1] = 1;
+        ans[2] = 1;
+        for(int i = 3 ; i <= n ; i++){
+            ans[i] = ans[i-1] + ans[i-2] + ans[i-3] ;
+        }
+        return ans[n];
+    }
+};
