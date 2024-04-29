fb74d956615ef5d13d4a5fbfdeb9e74d625a980b
Abhay Singh <109303835+abhay-59@users.noreply.github.com>
2024-04-29 13:00:53 +0530

    Time: 110 ms (58.12%), Space: 91.4 MB (54.80%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
+    int solve(int x, int y)
+    {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            if (((x >> i) & 1) != ((y >> i) & 1)) {
+                count++;
+            }
+        }
+        return count;
+    }
     int minOperations(vector<int>& nums, int k) {
         int x=0;
         for(auto i:nums){
             x^=i;
         }
-        string sk = bitset<32> (k).to_string();
-        string sx = bitset<32> (x).to_string();
-        x=0;
-        for(int i=32;i>=0;i--){
-            if(sx[i]!=sk[i])x++;
-        }
-        return x;
+        return solve(x,k);
     }
 };
