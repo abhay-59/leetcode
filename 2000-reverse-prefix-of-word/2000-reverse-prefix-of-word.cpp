67706c02555e0148a2ca630af4b3c5d778da947d
Abhay Singh <109303835+abhay-59@users.noreply.github.com>
2024-05-01 12:18:46 +0530

    Time: 3 ms (42.21%), Space: 7.5 MB (65.62%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string reversePrefix(string& word, char ch) {
+        ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
         int ind=-1;
         for(int i=0;i<word.size();i++){
             if(word[i]==ch){
@@ -8,7 +9,6 @@ public:
                 break;
             }
         }
-        //ind=word.find(ch);
         if (ind==-1) return word;
         for(int i=0; i<=ind/2; i++)
             swap(word[i], word[ind-i]);
