d6655bbe8311f0b5809297c8c9587b71cd66722d
Abhay Singh <109303835+abhay-59@users.noreply.github.com>
2024-06-27 19:17:54 +0530

    Time: 101 ms (98.91%), Space: 70.5 MB (99.34%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -4,11 +4,8 @@ public:
         ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
         vector<int>f=edges[0];
         vector<int>s=edges[1];
-        int f0=f[0],f1=f[1],s0=s[0],s1=s[1];
-        if(f0==s0)return f0;
-        if(f0==s1)return f0;
-        if(f1==s0)return f1;
-        if(f1==s1)return f1;
+        if(f[0]==s[0] || f[0]==s[1])return f[0];
+        if(f[1]==s[0] || f[1]==s[1])return f[1];
         return -1;
     }
 };
