727da4ae7cf91dc06695e317ad07a561d0c53d00
Abhay Singh <109303835+abhay-59@users.noreply.github.com>
2024-06-27 19:13:46 +0530

    Time: 90 ms (99.34%), Space: 70.6 MB (92.91%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
         ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-        unordered_map<int,int>mp;
-        for(auto i:edges){
-            mp[i[0]]++;
-            mp[i[1]]++;
-        }
-       int max_count=0,res=-1; 
-        for(auto i:mp) { 
-            if (max_count<i.second) { 
-                res=i.first; 
-                max_count=i.second; 
-            } 
-        } 
-    return res; 
+        vector<int>f=edges[0];
+        vector<int>s=edges[1];
+        int f0=f[0],f1=f[1],s0=s[0],s1=s[1];
+        if(f0==s0)return f0;
+        if(f0==s1)return f0;
+        if(f1==s0)return f1;
+        if(f1==s1)return f1;
+        return -1;
     }
 };
