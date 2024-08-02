18e570ed5bb3886f18800d7531fdf44e9e22d443
Abhay Singh <109303835+abhay-59@users.noreply.github.com>
2024-08-02 15:49:05 +0530

    Time: 65 ms (73.08%), Space: 93.6 MB (6.88%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -5,24 +5,21 @@ public:
         for(auto i:nums){
             if(i==1)count++;
         }
-        //if (count == 0 || count == n) return 0;
-        vector<int>v,nums1=nums;
-        int cur=0;
-        for(int i=0;i<count;i++){
-            cur+=nums[i];
-        }
-        v.push_back(cur);
+        vector<int>nums1=nums;
         for(auto i:nums){
             nums1.push_back(i);
         }
         n=nums1.size();
-        cout<<n<<" "<<count;
+        int cur=0;
+        for(int i=0;i<count;i++){
+            cur+=nums[i];
+        }
+        int mx=0;
         for(int i=0;i<(n-count);i++){
             cur-=nums1[i];
             cur+=nums1[i + count];
-            v.push_back(cur);
+            mx=max(cur,mx);
         }
-        int mx=*max_element(v.begin(),v.end());
         return count-mx;
     }
 };
