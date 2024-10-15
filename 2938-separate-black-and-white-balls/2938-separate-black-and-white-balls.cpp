a92bf6325c0fefb56ea4e8f6a047434bc06e833e
Abhay Singh <109303835+abhay-59@users.noreply.github.com>
2024-10-16 03:34:04 +0530

    Time: 30 ms (77.32%), Space: 15.6 MB (76.03%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long ans=0,count=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='1')count++;
+            else{
+                ans+=count;
+                //count=0;
+            } 
+        }
+        return ans;
+    }
+};
